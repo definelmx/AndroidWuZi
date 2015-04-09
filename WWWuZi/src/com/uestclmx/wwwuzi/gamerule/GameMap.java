1af61c92ae2057ac9d3c537ffea0c8492e8d5c8a
definelmx <452090495@qq.com>
2015-04-09 19:32:00 +0800

    基本功能
    人人对战，人机对战

---
+++ WWWuZi/src/com/uestclmx/wwwuzi/gamerule/GameMap.java
@@ -0,0 +1,41 @@
+package com.uestclmx.wwwuzi.gamerule;
+
+/**
+ * ÆåÅÌÂß¼­µØÍ¼
+ * 
+ * @author ÃùÏþ
+ * 
+ */
+public class GameMap {
+	public final static int CHESSBOARD_SIZE = 15;
+	public final static int NONE = 0, BLACK = 1, BLACK_UN = 2, WHITE = 3,
+			WHITE_UN = 4;
+
+	public int[][] board;
+
+	public int[][] value;
+
+	public GameMap() {
+		board = new int[CHESSBOARD_SIZE][CHESSBOARD_SIZE];
+		value = new int[CHESSBOARD_SIZE][CHESSBOARD_SIZE];
+		clear();
+	}
+
+	public void clear() {
+		int i, j;
+		for (i = 0; i < CHESSBOARD_SIZE; i++)
+			for (j = 0; j < CHESSBOARD_SIZE; j++) {
+				board[i][j] = 0;
+				value[i][j] = 0;
+			}
+	}
+
+	public void clearvalue() {
+		int i, j;
+		for (i = 0; i < CHESSBOARD_SIZE; i++)
+			for (j = 0; j < CHESSBOARD_SIZE; j++) {
+				value[i][j] = 0;
+			}
+	}
+
+}
